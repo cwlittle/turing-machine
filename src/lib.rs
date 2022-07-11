aefc66faca5719b176379f479c13bacdc61e54b8
Charlie Little <cwlittle@utexas.edu>
2022-07-11 03:06:56 -0500

    Add fancier output

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::{Debug, Display, Formatter};
 
 pub struct State {
     transition: &'static dyn Fn(&Option<char>, &mut Tape) -> usize,
@@ -18,6 +19,59 @@ pub struct Tape {
 impl From<Vec<Option<char>>> for Tape {
     fn from(tape: Vec<Option<char>>) -> Self {
         Tape { index: 0, tape }
+    }
+}
+
+impl Display for Tape {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let tape = self.tape.clone();
+        let mut display_string = "".to_string();
+
+        tape.iter().enumerate().for_each(|(i, c)| {
+            if i == self.index {
+                display_string += "┏━━━━━┓";
+            } else {
+                display_string += "┌─────┐";
+            }
+        });
+        display_string += "\n";
+
+        tape.iter().enumerate().for_each(|(i, c)| {
+            let str = match c {
+                Some(c) => c.to_string(),
+                None => " ".to_string(),
+            };
+            if i == self.index {
+                display_string += format!("┃  {}  ┃", str).as_str();
+            } else {
+                display_string += format!("│  {}  │", str).as_str();
+            }
+        });
+        display_string += "\n";
+
+        tape.iter().enumerate().for_each(|(i, c)| {
+            if i == self.index {
+                display_string += "┗━━━━━┛";
+            } else {
+                display_string += "└─────┘";
+            }
+        });
+
+        write!(f, "{}", display_string)
+    }
+}
+
+impl Debug for Tape {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut debug_string = "".to_string();
+        for c in self.tape.iter() {
+            match c {
+                Some(c) => debug_string += &c.to_string(),
+                None => debug_string += &" ".to_string(),
+            }
+        }
+
+        write!(f, "{}", debug_string)
     }
 }
 
@@ -81,16 +135,22 @@ impl Machine {
     }
 
     pub fn run(mut self) -> Self {
+        println!("Running machine with tape: {:?}", self.tape);
         let mut next_state = 0;
         loop {
+            println!("Current state: {}", next_state);
             let state = self.states.get(&next_state).unwrap();
             let current_read = self.tape.read();
+            println!("{}", self.tape);
+            println!("");
             next_state = (state.transition)(&current_read, &mut self.tape);
             if next_state == self.halt_accept {
                 println!("Accepted");
+                println!("");
                 return self;
             } else if next_state == self.halt_reject {
                 println!("Rejected");
+                println!("");
                 return self;
             }
         }
